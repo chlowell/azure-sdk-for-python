4e95732fd58850e31c802dd53fe62b51e9acf02d
Charles Lowell <chlowe@microsoft.com>
2020-09-30 11:22:49 -0700

    copy shared code

--- sdk/keyvault/azure-keyvault-certificates/tests/_shared/preparer.py
+++ sdk/keyvault/azure-keyvault-certificates/tests/_shared/preparer.py
@@ -7,9 +7,7 @@ try:
 except ImportError:  # python < 3.3
     from mock import Mock
 
-from azure.core.credentials import AccessToken
-from azure.identity import EnvironmentCredential
-from devtools_testutils import AzureMgmtPreparer
+from devtools_testutils import AzureMgmtPreparer, KeyVaultPreparer, CachedResourceGroupPreparer
 
 
 class KeyVaultClientPreparer(AzureMgmtPreparer):
@@ -18,12 +16,21 @@ class KeyVaultClientPreparer(AzureMgmtPreparer):
         self._client_cls = client_cls
 
     def create_credential(self):
-        if self.is_live:
-            return EnvironmentCredential()
-
-        return Mock(get_token=lambda *_: AccessToken("fake-token", 0))
+        return self.test_class_instance.settings.get_azure_core_credentials()
 
     def create_resource(self, _, **kwargs):
         credential = self.create_credential()
         client = self._client_cls(kwargs.get("vault_uri"), credential, **self.client_kwargs)
         return {"client": client}
+
+
+class CachedKeyVaultPreparer(KeyVaultPreparer):
+    def __init__(self, *args, **kwargs):
+        super(CachedKeyVaultPreparer, self).__init__(*args, **kwargs)
+        self.set_cache(True, self.parameter_name)
+
+    def create_resource(self, name, **kwargs):
+        if self.is_live and self.resource_group_parameter_name not in kwargs:
+            rg_preparer = CachedResourceGroupPreparer()
+            _, kwargs = rg_preparer._prepare_create_resource(self.test_class_instance, **kwargs)
+        return super(CachedKeyVaultPreparer, self).create_resource(name, **kwargs)
--- sdk/keyvault/azure-keyvault-certificates/tests/_shared/preparer_async.py
+++ sdk/keyvault/azure-keyvault-certificates/tests/_shared/preparer_async.py
@@ -5,7 +5,7 @@
 from unittest.mock import Mock
 
 from azure.core.credentials import AccessToken
-from azure.identity.aio import EnvironmentCredential
+from azure.identity.aio import DefaultAzureCredential
 
 from .preparer import KeyVaultClientPreparer as _KeyVaultClientPreparer
 from .helpers_async import get_completed_future
@@ -14,6 +14,6 @@ from .helpers_async import get_completed_future
 class KeyVaultClientPreparer(_KeyVaultClientPreparer):
     def create_credential(self):
         if self.is_live:
-            return EnvironmentCredential()
+            return DefaultAzureCredential()
 
         return Mock(get_token=lambda *_: get_completed_future(AccessToken("fake-token", 0)))
